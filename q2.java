0409f873ac8fb72d0853e3400eaff8c1649942cc
URK24CS1261 <adhishm@karunya.edu.in>
2025-07-31 10:56:04 +0530

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,17 @@
+package exp3;
+import java.util.Scanner;
+public class q2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		 System.out.println("enter the no elements:");
+	       int n = sc .nextInt();
+		int arr[]= new int[n];
+	    System.out.println("enter the elements:");
+        for (int i =0;i<arr.length;i++) {
+        	arr[i] =sc .nextInt();       	
+        }
+
+	}
+
+}
